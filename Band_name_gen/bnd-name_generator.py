6596801c31929d3b82d0219be534c2031ba75f8a
Israelshecktar <iamshecktar1996@.com>
2023-11-20 17:21:08 +0100

    implemented a simple band generator

---
+++ Band_name_gen/bnd-name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Shecktar Band name Generator!")
+bnd = input("Please kindly provide the city from which you grew up from? ")
+pet = input("what is the name of your pet? ")
+print('Consider naming your band ' + bnd + '-' + pet)
